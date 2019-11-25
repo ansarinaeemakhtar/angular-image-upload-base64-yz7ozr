f4983acf6e69a4eea38c574883739641f9e7d240
ansarinaeemakhtar <momin.naeemakhtar@gmail.com>
2019-11-25 20:41:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-image-upload-base64-yz7ozr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-image-upload-base64-yz7ozr)
---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {FileUploaderComponent} from './file-uploader.component';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 5';
+}
